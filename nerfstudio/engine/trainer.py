96b41e84a077b73845f5602c69798a2547db8538
qstx <2031074519@qq.com>
2023-11-27 12:12:45 +0800

    always call save_dataparser_transform

--- nerfstudio/engine/trainer.py
+++ nerfstudio/engine/trainer.py
@@ -226,7 +226,8 @@ class Trainer:
         assert self.pipeline.datamanager.train_dataset is not None, "Missing DatsetInputs"
 
         # don't want to call save_dataparser_transform if pipeline's datamanager does not have a dataparser
-        if isinstance(self.pipeline.datamanager, VanillaDataManager):
+        # if isinstance(self.pipeline.datamanager, VanillaDataManager):
+        if True:
             self.pipeline.datamanager.train_dataparser_outputs.save_dataparser_transform(
                 self.base_dir / "dataparser_transforms.json"
             )
